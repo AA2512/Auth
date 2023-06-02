49ad640f07bbeb27ec741566490bb2fd6e7b55eb
Aditya Agrawal <adityaagr00@gmail.com>
2023-06-02 12:58:15 +0530

    add auth

--- app.js
+++ app.js
@@ -1,53 +1,37 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 const mongoose = require("mongoose");
-
 const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+require("./models/User");
 
 const app = express();
+
+connectDB();
+require("./config/passport");
 
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 app.use(express.json());
 
-// clientID - 1096204226532-ktn75c7brhf7vt6ugs2na30ptg3rhkod.apps.googleusercontent.com
-// clientSecret - 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
-
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID:
-        "1096204226532-ktn75c7brhf7vt6ugs2na30ptg3rhkod.apps.googleusercontent.com",
-      clientSecret: "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",
-      callbackURL: "/auth/google/callback",
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log("Access Token: ", accessToken);
-      console.log("Refresh Token: ", refreshToken);
-      console.log("Profile: ", profile);
-    }
-  )
-);
-
-app.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"],
+app.use(
+  session({
+    secret: "mysecretkey",
+    resave: false,
+    saveUninitialized: true,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
   })
 );
 
-app.get("/auth/google/callback", passport.authenticate("google"));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use("/auth", require("./routes/auth"));
+app.use("/", require("./routes/index"));
 
 app.listen(3000, () => {
   console.log("Server is running on Port 3000");
 });
-
-// https://accounts.google.com/o/oauth2/v2/auth/oauthchooseaccount?
-// response_type=code&
-// redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fgoogle%2Fcallback&
-//scope=profile%20email
-// &client_id=1096204226532-ktn75c7brhf7vt6ugs2na30ptg3rhkod.apps.googleusercontent.com&service=lso&o2v=2&flowName=GeneralOAuthFlow
-
-// http://localhost:3000/auth/google/callback?code=4%2F0AbUR2VNFB3QafQkAZqkUhq5oOUwahvCrfUciLqzk9Bra8xezzviyia8VdrXCOQOst0A7Tg&scope=email+profile+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+openid&authuser=0&prompt=consent
--- models/User.js
+++ models/User.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
+  googleID: {
+    type: String,
+    required: true,
+  },
   firstName: {
     type: String,
     required: true,
@@ -9,17 +13,47 @@ const UserSchema = new mongoose.Schema({
     type: String,
     default: "",
   },
-  gender: {
+  displayName: {
     type: String,
-    required: true,
+    default: "",
   },
-  username: {
+  email: {
     type: String,
     required: true,
+    lowercase: true,
+    unique: true,
+    trim: true,
   },
   role: {
     type: Number, // 0-> creator, 1-> student
-    required: true,
+  },
+  image: {
+    type: String,
+    default: "",
+  },
+  cover: {
+    type: String,
+    default: "",
+  },
+  gender: {
+    type: String,
+    default: "",
+  },
+  about: {
+    type: String,
+    default: "",
+  },
+  birthdate: {
+    type: String,
+    default: "",
+  },
+  phone: {
+    type: String,
+    default: "+91 1234567890",
+  },
+  createdAt: {
+    type: Number,
+    default: Date.now(),
   },
 });
 
